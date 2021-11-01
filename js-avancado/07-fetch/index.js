2f5976c617c38686a35833ead22d2797bb0f0393
Roberto Dias <roberto.rhd@gmail.com>
2021-11-01 18:30:29 -0300

    adicionado o fetch usando api do github

---
+++ js-avancado/07-fetch/index.js
@@ -0,0 +1,19 @@
+const profile_image = document.getElementById('profile-image'); 
+let profile_name = document.getElementById('profile-name'); 
+let profile_description = document.getElementById('profile-description'); 
+let profile_username = document.getElementById('profile-username'); 
+
+async function getUser(user){
+    const response = await fetch(`${url}/${user}?client_id=${client_id}&client_secret=${client_secret}`)
+    return await response.json()
+}
+
+const setProfileData = (data) => {    
+    profile_image.src = data.avatar_url ? data.avatar_url : 'Não possui'
+    profile_name.innerHTML = data.name ? data.name : 'Não possui'
+    profile_description.innerHTML = data.bio ?? 'Não possui'
+    profile_username.innerHTML = data.login ?? 'Não possui'
+}
+
+const result = getUser('roberto-hofstetter')
+result.then(data => setProfileData(data))
---
+++ js-avancado/07-fetch/secrets.js
@@ -0,0 +1,3 @@
+const client_id = '8b02900ec847ad188131'
+const client_secret = 'bb643f80341d99db928bafc710dbf6e48ba1d2cf '
+const url = 'https://api.github.com/users'
